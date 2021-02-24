9c5c88e16b55a95ebd3476ffa16cb9c532d5e5b3
Olaf <olafdsouza27@gmail.com>
2021-02-24 09:16:06 -0500

    added leaderboard

--- Snakebot.py
+++ Snakebot.py
@@ -4,29 +4,44 @@ from discord.ext import commands
 
 f = open('data.json', 'r+')
 data = json.loads(f.read())
-bot = commands.Bot(command_prefix = "sb/")
+bot = commands.Bot(command_prefix="sb/")
 
 
 @bot.command()
-async def snake(ctx, target : discord.Member):
-    if(target.id not in data):
-        data[target.id] = 1
+async def snake(ctx, target: discord.Member):
+    if(str(target.name) not in data):
+        data[target.name] = 1
     else:
-        data[target.id] = data[target.id] + 1
-
+        data[target.name] = int(data[target.name]) + 1
     await ctx.send(target.name + "'s snakery has been noted")
+    with open('data.json', 'w') as storage:
+        json.dump(data, storage)
 
-@bot.command(aliases = ["snakecount", "Snakecount", "SnakeCount"])
-async def getSnakeCount(ctx, target : discord.Member):
-    await ctx.send(target.name + " has snaked people [" + data[target.name] + "]")
+
+@ bot.command(aliases=["snakecount", "Snakecount", "SnakeCount"])
+async def getSnakeCount(ctx, target: discord.Member):
+    await ctx.send(target.name + " has snaked people " + str(data[target.name]) + " times")
+
 
 @bot.command()
-async def stop(ctx, target : discord.Member):
+async def leaderboard(ctx):
+    snakes = {}
+    order = data.copy()
+    while len(order) > 0:
+        my_biggest_key = max(order, key=order.get)
+        snakes[my_biggest_key] = order[my_biggest_key]
+        order.pop(my_biggest_key)
+    await ctx.send(snakes)
+
+
+@ bot.command()
+async def stop(ctx, target: discord.Member):
     if (ctx.id == 363396359841251328):
         await ctx.send("Cya")
         f.write(json.dumps(data))
         f.close()
         bot.close()
         exit()
-
-bot.run("ODEzMjE5NzU2Mzc5MjA5NzUw.YDMH6g.28B7F3kXly69LRn_q5QB_jl4KyA")
+token = open('token.txt', 'r')
+botToken = token.read()
+bot.run(botToken)
